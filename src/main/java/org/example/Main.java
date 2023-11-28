231b54d424aa56587669ba2e69990765c3829728
Juan <juan16david19@gmail.com>
2023-11-28 11:30:38 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    //Natillera la abuelita toretto
+    // ¿ Que necesito para procesar la natillera de 1 cliente?
+    //Entradas
+    int identificadorCliente=23455;
+    long documentoIdentidad=10172544935665L;
+    byte edadCliente=34;
+    short cuotaMansualcliente=25000;
+    float tasaINteresFijo=0.1F;
+    char calificacionCliente='a';
+
+}
